57c91ad53445646c7436c463a1a290b516b3d86b
Sumedh Bala <sumedhbala@gmail.com>
2019-04-29 19:30:44 -0700

    job dsl

--- jenkins/jobs/tryjob.groovy
+++ jenkins/jobs/tryjob.groovy
@@ -1,8 +1,19 @@
-pipelineJob('try') {
-    definition {
-        cps {
-            script(readFileFromWorkspace('jenkins/jobs/try.groovy'))
-            sandbox()
-        }
+import static groovy.io.FileType.FILES;
+job('ci') {
+    description 'Build and test the app.'
+    scm {
+        github 'sumedhbala/InfraTest'
     }
+    steps {
+        def list = []
+        def dir = new File("/tmp")
+                    dir.eachFileRecurse (FILES) { file ->
+                        list << file
+                    }
+        list.each {
+                        println it.path
+                    }
+    }
+    
 }
+        
